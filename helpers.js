6cd7a65690ac86b3580c0837baefe173083b954f
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-06 01:17:46 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,29 +1,17 @@
-/*
-# Copyright (C) 2020 MuhammedKpln.
-#
-# WhatsAsena is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# WhatsAsena is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
 */
 
 function successfullMessage(msg) {
-    return "✅ *𝓦𝓗𝓘𝓣𝓔 𝓓𝓔𝓥𝓘𝓛*:  ```" + msg + "```"
+    return "👩‍🦰 *AMAZONE ALEXA*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *𝓦𝓗𝓘𝓣𝓔 𝓓𝓔𝓥𝓘𝓛*:  ```" + msg + "```"
+    return "🚀 *AMAZONE ALEXA*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *𝓦𝓗𝓘𝓣𝓔 𝓓𝓔𝓥𝓘𝓛*:  ```" + msg + "```"
+    return "🤖 *AMAZONE ALEXA*:  ```" + msg + "```"
 }
 
 
