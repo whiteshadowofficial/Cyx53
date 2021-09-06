4257307cb168685204dad1b9410ef80e9f987f14
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-07 01:14:02 +0530

    Update voicy.js

--- plugins/voicy.js
+++ plugins/voicy.js
@@ -1,3 +1,10 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+
 const Asena = require('../events');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
