ca631a43c27b0135965f609763de87e707314ab8
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-06 13:14:32 +0530

    Create inboxBlock.js

---
+++ plugins/inboxBlock.js
@@ -0,0 +1,58 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+const Neotro = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+
+//Inbox Block System By Queen Amazone
+
+const INBO = ""
+ if (Config.INBO == 'true') {
+Neotro.addCommand({on: 'text', fromMe: false, onlyPm: true }, (async (message, match) => {
+        let regexb1ichu = new RegExp('.')
+        let regexb2ichu = new RegExp('a')
+        let regexb3ichu = new RegExp('v')
+        let regexb4ichu = new RegExp('h')
+        let regexb5ichu = new RegExp('H')
+        let regexb6ichu = new RegExp('s')
+          if (regexb1ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          } 
+        else if (regexb2ichu.test(message.message)) {
+          
+           await message.client.sendMessage(message.jid, '*' + INBO + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+         else if (regexb3ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+        else if (regexb4ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+          else if (regexb5ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+          else if (regexb6ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + INBO + '*', MessageType.text);
+            await message.client.blockUser(message.jid, "add");
+          }
+          
+}));
+
+}
