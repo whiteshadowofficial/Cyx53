78b448b53f33015854ff2c450895618202a0690f
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-07 01:00:41 +0530

    Update greetings.js

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -1,9 +1,7 @@
-/* Copyright (C) 2021 TERROR BOY.
-
+/* Copyright (C) 2021 TENUX-Neotro.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-WhiteDevil TERROR BOY
+NEOTROX - TEENUHX
 */
 
 const Asena = require('../events');
