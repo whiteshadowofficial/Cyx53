6d52ef90636eb92a227ffdba84b0434ada0fc7a9
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-07 14:10:55 +0530

    Update bgmchanger.js

--- plugins/bgmchanger.js
+++ plugins/bgmchanger.js
@@ -1,5 +1,7 @@
-/* Copyright (C) 2020 afnanplk.
-re edited by terror-boy
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
 */
 
 const Asena = require('../events');
