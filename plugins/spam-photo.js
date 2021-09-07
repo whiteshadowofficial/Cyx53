2070e68b2e986d7aaf990c415a30d0dcb6901643
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-07 13:42:25 +0530

    Update and rename photospam.js to spam-photo.js

--- plugins/spam-photo.js
+++ plugins/spam-photo.js
@@ -1,14 +1,19 @@
-const Asena = require('../events');
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+const Neotro = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const {execFile} = require('child_process');
 const cwebp = require('cwebp-bin');
-
 const Language = require('../language');
 const Lang = Language.getString('spammer');
 
-Asena.addCommand({pattern: 'photo spam$', fromMe: true, desc: Lang.FOTO_DESC}, (async (message, match) => {
+Neotro.addCommand({pattern: 'photo spam$', fromMe: true, desc: Lang.FOTO_DESC}, (async (message, match) => {
     
     if (!message.reply_message) return await message.client.sendMessage(message.jid, Lang.FOTO_FOT, MessageType.text);
 
