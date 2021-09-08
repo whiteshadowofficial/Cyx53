c2258f9e0b3d35ad2e1a2660220e4566c8677231
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-08 11:46:18 +0530

    Update wame.js

--- plugins/wame.js
+++ plugins/wame.js
@@ -1,4 +1,11 @@
-const Asena = require('../events');
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+
+const Neotro = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 const fs = require('fs');
@@ -7,7 +14,7 @@ const WAME_DESC = "Get a link to the user chat."
 const WAME = "```Chat link from```@{}: https://wa.me/{}"
 const NEED_UWONG = "*Give me a user!*"
     
-    Asena.addCommand({pattern: 'wame ?(.*)', fromMe: false, desc: WAME_DESC}, (async (message, match) => {    
+    Neotro.addCommand({pattern: 'wame ?(.*)', fromMe: false, desc: WAME_DESC}, (async (message, match) => {    
         if (message.reply_message !== false) {
             await message.client.sendMessage(message.jid, WAME.format(message.reply_message.jid.split('@')[0], message.reply_message.jid.replace('@s.whatsapp.net', ' ')), MessageType.text, {
                 quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
