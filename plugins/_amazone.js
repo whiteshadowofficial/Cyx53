64c4d4098dc6d5b2fa96a21ff9668493c5c1330b
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-08 11:54:44 +0530

    Update _amazone.js

--- plugins/_amazone.js
+++ plugins/_amazone.js
@@ -9,7 +9,7 @@ const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
 const fs = require("fs")
 const Language = require('../language');
-const ams = fs.readFileSync('.Alexa/amazine.mp3')
+const ams = fs.readFileSync('src/image/amazine.mp3')
 const Lang = Language.getString('_amazone');
 
 if (Config.WORKTYPE == 'private') {
