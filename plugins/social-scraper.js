64b0b216a99a1e663b0ac892de457482f21ddfe2
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-09 18:37:30 +0530

    Update social-scraper.js

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -1,182 +1,195 @@
-/* Codded by @phaticusthiccy
-Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/kyrie.baran
-*/
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const Config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('scrapers');
+const capt = "🎭 *NeotroX* 🕊"
 
-const Asena = require('../events')
-const { MessageType } = require('@adiwajshing/baileys')
-const axios = require('axios')
-const cn = require('../config');
-
-const Language = require('../language')
-const { errorMessage, infoMessage } = require('../helpers')
-const Lang = Language.getString('instagram')
-const Tlang = Language.getString('tiktok')
-
-if (cn.WORKTYPE == 'private') {
-
-    Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
+if (Config.WORKTYPE == 'public') {
 	
-        let urls = `https://api.xteam.xyz/dl/ig?url=${match[1]}&APIKEY=ab9942f95c09ca89`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
+Asena.addCommand({ pattern: 'pinsta ?(.*)', fromMe: false }, async (message, match) => {
 
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
+    const link = match[1]
 
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Photo* ")
 
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
-            }
-        });
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
 
-    }));
+				await axios
+					.get(`https://bx-hunter.herokuapp.com/api/igdownload?url=${link}&apikey=Ikyy69`)
+					.then(async(response) => {
+						const {
+							linkdownload,
+							status,
+						} = response.data
 
-    /*
-    Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: true, desc: Tlang.TİKTOK }, async (message, match) => {
+						const linkdata = await axios.get(linkdownload, {
+							responseType: 'arraybuffer'
+						})
 
-        const userName = match[1]
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.image, {
+								caption: Config.BOT_NAME,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO PHOTO FOUND*"),
+							)
+					})
+					
+}) /* pattern close*/
 
-        if (!userName) return await message.client.sendMessage(message.jid, Tlang.NEED, MessageType.text)
+Asena.addCommand({ pattern: 'vinsta ?(.*)', fromMe: false }, async (message, match) => {
 
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text)
+    const link = match[1]
 
-        await axios
-          .get(`https://shinoa-rest.herokuapp.com/dl/tiktok?link=${userName}`)
-          .then(async (response) => {
-            const {
-              data,
-            } = response.data
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Video* ")
 
-            const profileBuffer = await axios.get(data.mp4, {
-              responseType: 'arraybuffer',
-            })
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
 
-            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-              caption: 'Made by WhatsAsena',
-            })
-          })
-          .catch(
-            async (err) => await message.client.sendMessage(message.jid, Tlang.NOT + userName, MessageType.text),
-          )
-      },
-    )
-    */
-}
-else if (cn.WORKTYPE == 'public') {
+				await axios
+					.get(`https://bx-hunter.herokuapp.com/api/igdownload?url=${link}&apikey=Ikyy69`)
+					.then(async(response) => {
+						const {
+							linkdownload,
+							status,
+						} = response.data
 
-    Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
+						const linkdata = await axios.get(linkdownload, {
+							responseType: 'arraybuffer'
+						})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.video, {
+								caption: Config.BOT_NAME,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO VIDEO FOUND*"),
+							)
+					})
+					
+}) /* pattern close*/
+
+Asena.addCommand({ pattern: 'pinsta ?(.*)', fromMe: true }, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Photo* ")
+
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
+
+				await axios
+					.get(`https://bx-hunter.herokuapp.com/api/igdownload?url=${link}&apikey=Ikyy69`)
+					.then(async(response) => {
+						const {
+							linkdownload,
+							status,
+						} = response.data
+
+						const linkdata = await axios.get(linkdownload, {
+							responseType: 'arraybuffer'
+						})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.image, {
+								caption: Config.BOT_NAME,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO PHOTO FOUND*"),
+							)
+					})
+					
+}) /* pattern close*/
+
+Asena.addCommand({ pattern: 'vinsta ?(.*)', fromMe: true }, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Video* ")
+
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
+
+				await axios
+					.get(`https://bx-hunter.herokuapp.com/api/igdownload?url=${link}&apikey=Ikyy69`)
+					.then(async(response) => {
+						const {
+							linkdownload,
+							status,
+						} = response.data
+
+						const linkdata = await axios.get(linkdownload, {
+							responseType: 'arraybuffer'
+						})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.video, {
+								caption: Config.BOT_NAME,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO VIDEO FOUND*"),
+							)
+					})
+					
+}) /* pattern close*/
+
+} /* public close*/
+
+else if (Config.WORKTYPE == 'private') {
 	
-        let urls = `https://api.xteam.xyz/dl/ig?url=${match[1]}&APIKEY=ab9942f95c09ca89`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
+	Asena.addCommand({ pattern: 'pinsta ?(.*)', fromMe: true }, async (message, match) => {
 
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
+    const link = match[1]
 
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Photo* ")
 
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
-            }
-        });
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
 
-    }));
-    Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: true, desc: Lang.DESC, dontAddCommandList: true }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-	
-        let urls = `https://api.xteam.xyz/dl/ig?url=${match[1]}&APIKEY=ab9942f95c09ca89`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
+				await axios
+					.get(`https://bx-hunter.herokuapp.com/api/igdownload?url=${link}&apikey=Ikyy69`)
+					.then(async(response) => {
+						const {
+							linkdownload,
+							status,
+						} = response.data
 
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
+						const linkdata = await axios.get(linkdownload, {
+							responseType: 'arraybuffer'
+						})
 
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.image, {
+								caption: Config.BOT_NAME,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO PHOTO FOUND*"),
+							)
+					})
+					
+}) /* pattern close*/
 
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
-            }
-        });
+Asena.addCommand({ pattern: 'vinsta ?(.*)', fromMe: true }, async (message, match) => {
 
-    }));
-    /*
-    Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: false, desc: Tlang.TİKTOK }, async (message, match) => {
+    const link = match[1]
 
-        const userName = match[1]
+    if (!link) return await message.sendMessage(" *Give Vaild Insta Link That Includes Video* ")
 
-        if (!userName) return await message.client.sendMessage(message.jid, Tlang.NEED, MessageType.text)
+    await message.sendMessage('👻 *Insta Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
 
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text)
+				await axios
+					.get(`https://bx-hunter.herokuapp.com/api/igdownload?url=${link}&apikey=Ikyy69`)
+					.then(async(response) => {
+						const {
+							linkdownload,
+							status,
+						} = response.data
 
-        await axios
-          .get(`https://api.xteam.xyz/dl/tiktok?url=${userName}&APIKEY=ab9942f95c09ca89`)
-          .then(async (response) => {
-            const {
-              server_1,
-            } = response.data
+						const linkdata = await axios.get(linkdownload, {
+							responseType: 'arraybuffer'
+						})
 
-            const profileBuffer = await axios.get(server_1, {
-              responseType: 'arraybuffer',
-            })
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.video, {
+								caption: Config.BOT_NAME,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID LINK OR NO VIDEO FOUND*"),
+							)
+					})
+					
+}) /* pattern close*/
 
-            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-              caption: 'Made by WhatsAsena',
-            })
-          })
-          .catch(
-            async (err) => await message.client.sendMessage(message.jid, Tlang.NOT + userName, MessageType.text),
-          )
-      },
-    )
-    Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: true, desc: Tlang.TİKTOK }, async (message, match) => {
-
-        const userName = match[1]
-
-        if (!userName) return await message.client.sendMessage(message.jid, Tlang.NEED, MessageType.text)
-
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text)
-
-        await axios
-          .get(`https://shinoa-rest.herokuapp.com/dl/tiktok?link=${userName}`)
-          .then(async (response) => {
-            const {
-              data,
-            } = response.data
-
-            const profileBuffer = await axios.get(data.mp4, {
-              responseType: 'arraybuffer',
-            })
-
-            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-              caption: 'Made by WhatsAsena',
-            })
-          })
-          .catch(
-            async (err) => await message.client.sendMessage(message.jid, Tlang.NOT + userName, MessageType.text),
-          )
-      },
-    )
-    */
-}
+} /* private close*/
