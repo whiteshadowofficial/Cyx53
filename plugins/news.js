1356bcd4c2448ce211d44207d6d316818ba30335
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-10 23:47:53 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -38,10 +38,10 @@ Neotro.addCommand({on: 'text', fromMe: td, deleteCommand: false}, (async (messag
          let regex18 = new RegExp('YASH')
          let regex19 = new RegExp('UASH')
           if (regex1.test(message.message)) {              
-             await message.client.sendMessage(message.jid,'\n👩‍🦰', MessageType.text, {quoted: message.data })
+             await message.client.sendMessage(message.jid,'*👩‍🦰Amazone Alexa Anime List*\n\n╔═════════▷\n╠🌼: .loli\n╠🌼: .wifu\n╠🌼: .neko\n╠🌼: .ava\n╠🌼: .kemo\n╠🌼: .awoo\n╠🌼: .shinobu\n╠🌼: .megumin\n╚═════════▷', MessageType.text, {quoted: message.data })
          } 
          else if (regex2.test(message.message)) {
-            await message.client.sendMessage(message.jid,'*👩‍🦰Amazone Alexa Anime List*\n\n╔═════════▷\n╠🌼: .loli\n╠🌼: .wifu\n╠🌼: .neko\n╠🌼: .ava\n╠🌼: .kemo\n╠🌼: .awoo\n╠🌼: .shinobu\n╠🌼: .megumin\n╚═════════▷' , MessageType.text, {quoted: message.data })
+            await message.client.sendMessage(message.jid,'*╔═════◉ᴀᴍᴀᴢᴏɴᴇ◉═════╗*\n           👩‍🦰ǫᴜᴇᴇɴ ᴀᴍᴀᴢᴏɴᴇ 🪐\n*❖═මීම් ලැයිස්තුව ᴍᴇᴍᴇ ᴘᴀᴄᴋ═❖*\n\n*💠.yasai       ❴your text❵*\n*💠.uddika    ❴your text❵*\n*💠.aah          ❴your text❵*\n*💠.sir            ❴your text❵*\n*💠.my3         ❴your text❵*\n*💠.fuck         ❴your text❵*\n*💠.hii             ❴your text❵*\n*💠.asai          ❴your text❵*\n*💠.hapoi       ❴your text❵*\n*💠.anura       ❴your text❵*\n*💠.gemba       ❴your text❵*\n*💠.wow       ❴your text❵*\n*💠.hapo       ❴your text❵*\n*💠.babah       ❴your text❵*\n*💠.mcn       ❴your text❵*\n*💠.nah       ❴your text❵*\n*╚═══❖══▣══▣══❖═══╝*\n\n*▷Template Credit: එ.මි.ස ¡v*' , MessageType.text, {quoted: message.data })
          }
           else if (regex3.test(message.message)) {
              await message.client.sendMessage(message.jid,'', MessageType.text, {quoted: message.data })
