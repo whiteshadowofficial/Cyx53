2c28178a00d7b5f8b3c7bb75703fada9ff244218
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-10 14:52:06 +0530

    Update Esthetic.js

--- plugins/Esthetic.js
+++ plugins/Esthetic.js
@@ -1,13 +1,22 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
 const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const Ln = "Esthetic Photos"
 const axios = require('axios');
 const Config = require('../config');
+let Language = require('../language');
+let Lang = Language.getString('amazone');
+
 
 if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'esthetic ?(.*)', fromMe: false,dontaddCommandList: false }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'esthetic ?(.*)', fromMe: false, Lang.WALL }, (async (message, match) => {
       
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/estetikpic?apikey=THEVINDUapi`, { responseType: 'arraybuffer' })
 
@@ -19,7 +28,7 @@ if (Config.WORKTYPE == 'public') {
 
 else if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'esthetic ?(.*)', fromMe: true,dontaddCommandList: false }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'esthetic ?(.*)', fromMe: true, Lang.WALL }, (async (message, match) => {
       
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/estetikpic?apikey=THEVINDUapi`, { responseType: 'arraybuffer' })
 
