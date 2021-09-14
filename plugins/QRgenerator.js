121ea1710159c6599470fa1f5a3c85a2c1c18e3a
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-14 22:48:32 +0530

    Update and rename qrgenerator.js to QRgenerator.js

--- plugins/QRgenerator.js
+++ plugins/QRgenerator.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 terror-boy.
+/* Copyright (C) 2021 TENUX-Neotro.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-White Devil-terror boy 
+NEOTROX - TEENUHX
 */
 
 const Asena = require('../events');
