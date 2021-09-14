7a86971610936b946d373a90cb87986792670841
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-14 21:51:49 +0530

    Update Dcsong.js

--- plugins/Dcsong.js
+++ plugins/Dcsong.js
@@ -1,9 +1,7 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
+/* Copyright (C) 2021 TENUX-Neotro.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
+NEOTROX - TEENUHX
 */
 
 const Asena = require('../events');
