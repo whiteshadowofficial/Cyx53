16d96cdb1c9535bf4f34b26c59aefbc0f4611c92
Sayuri556 <88507137+jesonpro@users.noreply.github.com>
2021-09-14 23:26:53 +0530

    Create MEDIALIST.js

---
+++ plugins/MEDIALIST.js
@@ -0,0 +1,23 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+const Neotro = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Neotro.addCommand({pattern: '', fromMe: fs, desc: Lang.ALIST}, (async (message, match) => {
+    await message.sendMessage("");
+
+}));
+
+Neotro.addCommand({pattern: '', fromMe: fs, desc: Lang.ALIST}, (async (message, match) => {
+    await message.sendMessage("");
+
+}));
+
+Neotro.addCommand({pattern: '', fromMe: fs, desc: Lang.ALIST}, (async (message, match) => {
+    await message.sendMessage("");
+
+}));
